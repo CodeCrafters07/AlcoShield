3d2f3f4dcce4a852897289a8a48087f5ff7581b5
issa99-lab <issanana99@gmail.com>
2024-02-25 23:36:57 +0300

    Figuring out how to set up the constructor when testing

--- Tests/test/QR.t.sol
+++ Tests/test/QR.t.sol
@@ -7,7 +7,20 @@ import {QrCode} from "../src/QR.sol";
 contract CounterTest is Test {
     QrCode public qrContract;
 
+    struct SystemOwner {
+        address sysOwner;
+        bytes32 password /*audit*/;
+        bool isLogin;
+    }
+    SystemOwner public sysowner;
+    sysowner = SystemOwner(owner, keccak256(abi.encode(("admin"))), false);
+    address public owner;
+    owner = msg.sender;
+    sysOwnerMap[owner] = sysowner;
+
+    mapping(address => SystemOwner) private sysOwnerMap;
+
     function setUp() public {
-        qrContract = new QrCode();
+        qrContract = new QrCode(owner, sysowner, sysOwnerMap[owner]);
     }
 }
